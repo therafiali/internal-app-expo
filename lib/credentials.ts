5ce261d1b6e215cdbfc2f312b71e387c3c681c55
Muhammad Rafi Ali <therafiali@gmail.com>
2025-07-23 14:34:33 +0500

    apply OS based condtions

--- lib/credentials.ts
+++ lib/credentials.ts
@@ -1,6 +1,15 @@
 import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
 
 export const getCredientials = async () => {
+  if (Platform.OS === "web") {
+    const username = localStorage.getItem("username");
+    const password = localStorage.getItem("password");
+    if (!username || !password) {
+      throw new Error("Username or password is not set");
+    }
+    return { username, password };
+  }
   const username = await SecureStore.getItemAsync("username");
   const password = await SecureStore.getItemAsync("password");
   if (!username || !password) {
@@ -10,6 +19,11 @@ export const getCredientials = async () => {
 };
 
 export const setCredientials = async (username: string, password: string) => {
+  if (Platform.OS === "web") {
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+    return;
+  }
   await SecureStore.setItemAsync("username", username);
   await SecureStore.setItemAsync("password", password);
 
